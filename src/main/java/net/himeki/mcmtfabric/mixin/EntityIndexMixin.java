bc7a9f52fa3ec7f0feb97e9ce7abdd88acba7519
Grider <himekifee@gmail.com>
2021-07-15 10:57:12 +0800

    Try to resolve a npe

--- src/main/java/net/himeki/mcmtfabric/mixin/EntityIndexMixin.java
+++ src/main/java/net/himeki/mcmtfabric/mixin/EntityIndexMixin.java
@@ -1,10 +1,25 @@
 package net.himeki.mcmtfabric.mixin;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.himeki.mcmtfabric.parallelised.ConcurrentCollections;
+import net.himeki.mcmtfabric.parallelised.fastutil.Int2ObjectConcurrentHashMap;
 import net.minecraft.world.entity.EntityIndex;
 import net.minecraft.world.entity.EntityLike;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Map;
+import java.util.UUID;
 
 @Mixin(EntityIndex.class)
 public class EntityIndexMixin<T extends EntityLike> {
+    @Shadow
+    @Final
+    private final Int2ObjectMap<T> idToEntity = new Int2ObjectConcurrentHashMap();
+
+    @Shadow
+    @Final
+    private final Map<UUID, T> uuidToEntity = ConcurrentCollections.newHashMap();
 
 }
