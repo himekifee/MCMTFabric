578e9f6d52a7449fdce31f0d3b35fa4d08386d07
Grider <himekifee@gmail.com>
2022-02-12 23:14:49 +0000

    Use internal lib mixin impl

--- src/main/java/net/himeki/mcmtfabric/syncfu/SyncFuTransformer.java
+++ src/main/java/net/himeki/mcmtfabric/syncfu/SyncFuTransformer.java
@@ -1,49 +1,23 @@
 package net.himeki.mcmtfabric.syncfu;
 
-import net.devtech.grossfabrichacks.entrypoints.PrePreLaunch;
-import net.devtech.grossfabrichacks.instrumentation.InstrumentationApi;
-import net.devtech.grossfabrichacks.jarboot.JarBooter;
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.fabricmc.loader.impl.launch.knot.Knot;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.net.URL;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Optional;
-
-public class SyncFuTransformer implements PrePreLaunch {
+public class SyncFuTransformer implements PreLaunchEntrypoint {
     private static final Logger syncFuTransformerLogger = LogManager.getLogger();
     private boolean isActive = true;
 
     @Override
-    public void onPrePreLaunch() {
-        syncFuTransformerLogger.info("On SyncFuTransformer PrePreLaunch...");
-
-        Optional<URL> fuJarUrl = Arrays.stream(System.getProperty("java.class.path").split(File.pathSeparator)).flatMap(path -> {
-            File file = new File(path);
-            if (file.isDirectory()) {
-                return Arrays.stream(file.list((d, n) -> n.endsWith(".jar")));
-            }
-            return Arrays.stream(new String[]{path});
-        })
-                .filter(p -> p.contains("fastutil")) // Can add more if necesary;
-                .map(Paths::get)
-                .map(path -> {
-                    try {
-                        return path.toUri().toURL();
-                    } catch (Exception e) {
-                        return null;
-                    }
-                }).findFirst();
-        if (fuJarUrl.isPresent()) {
-            syncFuTransformerLogger.info("Sync_Fu found fu...");
-            if (isActive) {
-                JarBooter.addUrl(fuJarUrl.get());
-                InstrumentationApi.pipeClassThroughTransformerBootstrap("it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap");
-                InstrumentationApi.pipeClassThroughTransformerBootstrap("it.unimi.dsi.fastutil.longs.LongLinkedOpenHashSet");
-                InstrumentationApi.pipeClassThroughTransformerBootstrap("it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap");
-            }
-        } else syncFuTransformerLogger.warn("Failed to find FastUtil jar; this WILL result in more exceptions");
+    public void onPreLaunch() {
+        syncFuTransformerLogger.info("On SyncFuTransformer PreLaunch...");
+        try {
+            Knot.getLauncher().loadIntoTarget("it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap");
+            Knot.getLauncher().loadIntoTarget("it.unimi.dsi.fastutil.longs.LongLinkedOpenHashSet");
+            Knot.getLauncher().loadIntoTarget("it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
