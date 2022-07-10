bc27a84f62662669eb685fdab01ef9df652ab515
Grider <himekifee@gmail.com>
2022-07-10 13:20:42 +0100

    Fix concurrent scheduling of block updates cause residual OrderedTick in queuedTicks that prevent other ticks from been added

--- src/main/java/net/himeki/mcmtfabric/mixin/SyncAllMixin.java
+++ src/main/java/net/himeki/mcmtfabric/mixin/SyncAllMixin.java
@@ -1,8 +1,9 @@
 package net.himeki.mcmtfabric.mixin;
 
 import net.minecraft.entity.ai.pathing.PathMinHeap;
+import net.minecraft.world.tick.ChunkTickScheduler;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(PathMinHeap.class)
+@Mixin(value = {PathMinHeap.class, ChunkTickScheduler.class})
 public class SyncAllMixin {
 }
