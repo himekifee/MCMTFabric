ed8395af6222cdcf1decd03067db82a832d769c1
Grider <himekifee@gmail.com>
2022-08-09 21:22:02 +0100

    Mojang wants entity list to be safe? Daydreaming

--- src/main/java/net/himeki/mcmtfabric/mixin/EntityListMixin.java
+++ src/main/java/net/himeki/mcmtfabric/mixin/EntityListMixin.java
@@ -6,6 +6,9 @@ import net.minecraft.entity.Entity;
 import net.minecraft.world.EntityList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityList.class)
 public abstract class EntityListMixin {
@@ -14,4 +17,9 @@ public abstract class EntityListMixin {
 
     @Shadow
     private Int2ObjectMap<Entity> temp = new Int2ObjectConcurrentHashMap<>();
+
+    @Inject(method = "ensureSafe", at = @At(value = "HEAD"), cancellable = true)
+    private void notSafeAnyWay(CallbackInfo ci) {
+        ci.cancel();
+    }
 }
