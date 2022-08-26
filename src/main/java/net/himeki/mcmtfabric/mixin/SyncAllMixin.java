477bfaf3ee301266182800c0b5931ecc5693c27f
Grider <himekifee@gmail.com>
2022-08-26 18:26:14 +0100

    Seems LevelPropagator is still causing issues...

--- src/main/java/net/himeki/mcmtfabric/mixin/SyncAllMixin.java
+++ src/main/java/net/himeki/mcmtfabric/mixin/SyncAllMixin.java
@@ -1,9 +1,10 @@
 package net.himeki.mcmtfabric.mixin;
 
 import net.minecraft.entity.ai.pathing.PathMinHeap;
+import net.minecraft.world.chunk.light.LevelPropagator;
 import net.minecraft.world.tick.ChunkTickScheduler;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(value = {PathMinHeap.class, ChunkTickScheduler.class})
+@Mixin(value = {PathMinHeap.class, ChunkTickScheduler.class, LevelPropagator.class})
 public class SyncAllMixin {
 }
