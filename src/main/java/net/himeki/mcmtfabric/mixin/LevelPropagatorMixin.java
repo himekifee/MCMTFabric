7fdbe9addb17caa91c34f47503c0538c5dcc17a4
Grider <himekifee@gmail.com>
2022-11-18 15:11:11 +0000

    Sync is not enough. Revert to concurrent hash set

--- src/main/java/net/himeki/mcmtfabric/mixin/LevelPropagatorMixin.java
+++ src/main/java/net/himeki/mcmtfabric/mixin/LevelPropagatorMixin.java
@@ -1,8 +1,16 @@
 package net.himeki.mcmtfabric.mixin;
 
+import it.unimi.dsi.fastutil.longs.LongLinkedOpenHashSet;
+import net.himeki.mcmtfabric.parallelised.fastutil.ConcurrentLongLinkedOpenHashSet;
 import net.minecraft.world.chunk.light.LevelPropagator;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LevelPropagator.class)
 public abstract class LevelPropagatorMixin {
+    @Redirect(method = "<init>", at = @At(value = "FIELD", target = "Lnet/minecraft/world/chunk/light/LevelPropagator;pendingIdUpdatesByLevel:[Lit/unimi/dsi/fastutil/longs/LongLinkedOpenHashSet;", args = "array=set"))
+    private void overwritePendingIdUpdatesByLevel(LongLinkedOpenHashSet[] hashSets, int index, LongLinkedOpenHashSet hashSet, int levelCount, final int expectedLevelSize, final int expectedTotalSize) {
+        hashSets[index] = new ConcurrentLongLinkedOpenHashSet(expectedLevelSize, 0.5f);
+    }
 }
