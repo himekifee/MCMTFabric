aabbf922eb91d8c53c9dc67658254ecb9c19647f
Grider <himekifee@gmail.com>
2022-11-19 19:02:59 +0000

    Fix NPE when accessing cleared fields

---
+++ src/main/java/net/himeki/mcmtfabric/mixin/PathNodeNavigatorMixin.java
@@ -0,0 +1,8 @@
+package net.himeki.mcmtfabric.mixin;
+
+import net.minecraft.entity.ai.pathing.PathNodeNavigator;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PathNodeNavigator.class)
+public class PathNodeNavigatorMixin {
+}
