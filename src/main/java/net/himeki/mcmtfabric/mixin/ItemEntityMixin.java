192eeeafd342e02e42c0048d902aeedd710d84b7
Grider <himekifee@gmail.com>
2022-11-19 18:49:36 +0000

    Fix item lost when multiple merges happen on the same ItemStack. Fixes #39

---
+++ src/main/java/net/himeki/mcmtfabric/mixin/ItemEntityMixin.java
@@ -0,0 +1,25 @@
+package net.himeki.mcmtfabric.mixin;
+
+import net.minecraft.entity.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+@Mixin(ItemEntity.class)
+public class ItemEntityMixin {
+    private static final ReentrantLock lock = new ReentrantLock();
+
+    @Inject(method="tryMerge()V",at=@At(value="HEAD"))
+    private void lock(CallbackInfo ci) {
+        lock.lock();
+    }
+
+    @Inject(method="tryMerge()V",at=@At(value="RETURN"))
+    private void unlock(CallbackInfo ci) {
+        lock.unlock();
+    }
+
+}
