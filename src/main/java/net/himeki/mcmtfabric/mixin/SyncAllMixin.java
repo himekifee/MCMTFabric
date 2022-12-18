13b9e52ca447620cce0939d74e7f6d9213551bfc
Grider <himekifee@gmail.com>
2022-12-18 17:19:14 +0000

    Fix EntityNavigation NPE

--- src/main/java/net/himeki/mcmtfabric/mixin/SyncAllMixin.java
+++ src/main/java/net/himeki/mcmtfabric/mixin/SyncAllMixin.java
@@ -1,10 +1,11 @@
 package net.himeki.mcmtfabric.mixin;
 
+import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.ai.pathing.PathMinHeap;
 import net.minecraft.world.chunk.light.LevelPropagator;
 import net.minecraft.world.tick.ChunkTickScheduler;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(value = {PathMinHeap.class, ChunkTickScheduler.class, LevelPropagator.class})
+@Mixin(value = {PathMinHeap.class, ChunkTickScheduler.class, LevelPropagator.class, EntityNavigation.class})
 public class SyncAllMixin {
 }
