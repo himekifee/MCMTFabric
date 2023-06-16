1ca80d7e53e20c6d15dc53b573e87b7a2ace51d9
Grider <himekifee@gmail.com>
2023-06-16 15:09:32 +0100

    Fix NPEs in SimpleGameEventDispatcher, don't crash with allays

--- src/main/java/net/himeki/mcmtfabric/mixin/SyncAllMixin.java
+++ src/main/java/net/himeki/mcmtfabric/mixin/SyncAllMixin.java
@@ -3,11 +3,11 @@ package net.himeki.mcmtfabric.mixin;
 import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.ai.pathing.PathMinHeap;
 import net.minecraft.util.thread.LockHelper;
-import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.chunk.light.LevelPropagator;
+import net.minecraft.world.event.listener.SimpleGameEventDispatcher;
 import net.minecraft.world.tick.ChunkTickScheduler;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(value = {PathMinHeap.class, ChunkTickScheduler.class, LevelPropagator.class, EntityNavigation.class, LockHelper.class})
+@Mixin(value = {PathMinHeap.class, ChunkTickScheduler.class, LevelPropagator.class, EntityNavigation.class, LockHelper.class, SimpleGameEventDispatcher.class})
 public class SyncAllMixin {
 }
